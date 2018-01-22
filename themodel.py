4db0974231a4cf7557d809b47b3037f5f6c13345
Alp <alp@Alps-MacBook-Pro.local>
2018-01-22 02:10:55 +0100

    the series

---
+++ themodel.py
@@ -0,0 +1,326 @@
+# coding: utf-8
+from __future__ import division
+
+import theano
+from theano import tensor as T, function, printing
+import _pickle
+import os
+import numpy as np
+
+def PReLU(a, x):
+    return T.maximum(0.0, x) + a * T.minimum(0.0, x)
+
+def ReLU(x):
+    return T.maximum(0.0, x)
+
+def _get_shape(i, o, keepdims):
+    if (i == 1 or o == 1) and not keepdims:
+        return (max(i,o),)
+    else:
+        return (i, o)
+
+def _slice(tensor, size, i):
+    """Gets slice of columns of the tensor"""
+    if tensor.ndim == 2:
+        return tensor[:, i*size:(i+1)*size]
+    elif tensor.ndim == 1:
+        return tensor[i*size:(i+1)*size]
+    else:
+        raise NotImplementedError("Tensor should be 1 or 2 dimensional")
+
+def weights_const(i, o, name, const, keepdims=False):
+    W_values = np.ones(_get_shape(i, o, keepdims)).astype(theano.config.floatX) * const
+    return theano.shared(value=W_values, name=name, borrow=True)
+
+def weights_identity(i, o, name, const, keepdims=False):
+    #"A Simple Way to Initialize Recurrent Networks of Rectified Linear Units" (2015) (http://arxiv.org/abs/1504.00941)
+    W_values = np.eye(*_get_shape(i, o, keepdims)).astype(theano.config.floatX) * const
+    return theano.shared(value=W_values, name=name, borrow=True)
+
+def weights_Glorot(i, o, name, rng, is_logistic_sigmoid=False, keepdims=False):
+    #i: no of levels
+    #o: output vector size
+    #http://jmlr.org/proceedings/papers/v9/glorot10a/glorot10a.pdf
+    d = np.sqrt(6. / (i + o))
+    if is_logistic_sigmoid:
+        d *= 4.
+    W_values = rng.uniform(low=-d, high=d, size=_get_shape(i, o, keepdims)).astype(theano.config.floatX)
+    return theano.shared(value=W_values, name=name, borrow=True)
+
+class GRULayer(object):
+
+    def __init__(self, rng, n_in, n_out, minibatch_size):
+        super(GRULayer, self).__init__()
+        # Notation from: An Empirical Exploration of Recurrent Network Architectures
+
+        self.n_in = n_in
+        self.n_out = n_out
+
+        # Initial hidden state
+        self.h0 = theano.shared(value=np.zeros((minibatch_size, n_out)).astype(theano.config.floatX), name='h0', borrow=True)
+
+        # Gate parameters:
+        self.W_x = weights_Glorot(n_in, n_out*2, 'W_x', rng, keepdims=True)
+        self.W_h = weights_Glorot(n_out, n_out*2, 'W_h', rng, keepdims=True)
+        self.b = weights_const(1, n_out*2, 'b', 0)
+        # Input parameters
+        self.W_x_h = weights_Glorot(n_in, n_out, 'W_x_h', rng, keepdims=True)
+        self.W_h_h = weights_Glorot(n_out, n_out, 'W_h_h', rng, keepdims=True)
+        self.b_h = weights_const(1, n_out, 'b_h', 0)
+
+        self.params = [self.W_x, self.W_h, self.b, self.W_x_h, self.W_h_h, self.b_h]
+
+    def step(self, x_t, h_tm1):
+
+        rz = T.nnet.sigmoid(T.dot(x_t, self.W_x) + T.dot(h_tm1, self.W_h) + self.b)
+        r = _slice(rz, self.n_out, 0)
+        z = _slice(rz, self.n_out, 1)
+
+        h = T.tanh(T.dot(x_t, self.W_x_h) + T.dot(h_tm1 * r, self.W_h_h) + self.b_h)
+
+        h_t = z * h_tm1 + (1. - z) * h
+
+        return h_t
+
+def load(file_path, minibatch_size):
+	import themodel
+	import _pickle
+	import theano
+	import numpy as np
+
+	with open(file_path, 'rb') as f:
+		state = _pickle.load(f)
+
+	Model = getattr(themodel, state["type"])
+	print(Model)
+
+	rng = np.random
+	rng.set_state(state["random_state"])
+
+	input_PuncTensors = []
+	inputs = []
+	input_feature_names = []
+	for tensor_info in state["input_tensors_info"]:
+		input_feature_names.append(tensor_info['name'])
+		if tensor_info['bidirectional']:
+			is_bidi = True
+		else:
+			is_bidi = False
+		if tensor_info['vocabularized']:
+			tensor = T.imatrix(tensor_info['name'])
+			vocabulary_size = tensor_info['vocabulary_size']
+			feature_PuncTensor = PuncTensor(name=tensor_info['name'], tensor=tensor, size_hidden=tensor_info['size_hidden'], size_emb=tensor_info['size_emb'], vocabularized=True, vocabulary_size=tensor_info['vocabulary_size'], bidirectional=is_bidi)
+		else:
+			tensor = T.matrix(tensor_info['name'])
+			feature_PuncTensor = PuncTensor(name=tensor_info['name'], tensor=tensor, size_hidden=tensor_info['size_hidden'], size_emb=tensor_info['size_emb'], vocabularized=False, vocabulary_size=tensor_info['vocabulary_size'], bidirectional=is_bidi)
+		inputs += tensor
+		input_PuncTensors.append(feature_PuncTensor)
+
+	net = Model(rng=rng,
+        minibatch_size=minibatch_size,
+        y_vocabulary_size=state["y_vocabulary_size"],
+        num_hidden_output=state["num_hidden_output"],
+        input_tensors=input_PuncTensors
+	)
+
+	for net_param, state_param in zip(net.params, state["params"]):
+		net_param.set_value(state_param, borrow=True)
+
+	gsums = [theano.shared(gsum) for gsum in state["gsums"]] if state["gsums"] else None
+
+	tensors = [i.tensor for i in input_PuncTensors]
+
+	return net, tensors, input_feature_names, (gsums, state["learning_rate"], state["validation_ppl_history"], state["epoch"], rng)
+	# return net
+
+class PuncTensor(object):
+	def __init__(self, name, tensor=None, size_hidden=0, size_emb=1, vocabularized=False, vocabulary_size = 0,  bidirectional=False):
+		#if vocabularized == False, size_emb has to be 1
+		self.name = name
+		self.tensor = tensor
+		self.bidirectional = bidirectional
+		self.vocabularized = vocabularized
+		self.vocabulary_size = vocabulary_size
+		self.size_hidden = size_hidden
+		self.size_emb = size_emb
+
+		self.GRUs = []
+
+		self.We = None
+
+	def initialize_layers(self, rng, minibatch_size):
+		if self.vocabularized:
+			self.We = weights_Glorot(self.vocabulary_size, self.size_emb, 'We_' + self.name, rng)
+
+		total_n_out = 0
+		if self.bidirectional:
+			forward_layer = GRULayer(rng=rng, n_in=self.size_emb, n_out=self.size_hidden, minibatch_size=minibatch_size)
+			backward_layer = GRULayer(rng=rng, n_in=self.size_emb, n_out=self.size_hidden, minibatch_size=minibatch_size)
+			self.GRUs.append(forward_layer)
+			self.GRUs.append(backward_layer)
+			total_n_out += self.size_hidden * 2
+		else:
+			unidirectional_layer = GRULayer(rng=rng, n_in=self.size_emb, n_out=self.size_hidden, minibatch_size=minibatch_size)
+			self.GRUs.append(unidirectional_layer)
+			total_n_out += self.size_hidden
+
+		return total_n_out
+
+	def is_empty(self):
+		if self.tensor == None:
+			return True
+		else:
+			return False
+
+	def mini_ver(self):
+		mini_version = PuncTensor(name=self.name, size_hidden=self.size_hidden, size_emb=self.size_emb, vocabularized=self.vocabularized, vocabulary_size=self.vocabulary_size, bidirectional=self.bidirectional)
+		return mini_version
+
+	def as_dict(self):
+		tensor_info = { 'name':self.name,
+						'size_hidden':self.size_hidden,
+						'size_emb':self.size_emb,
+						'vocabularized':self.vocabularized,
+						'vocabulary_size':self.vocabulary_size,
+						'bidirectional':self.bidirectional}
+		return tensor_info
+
+class GRU_parallel(object):
+	def __init__(self, rng, y_vocabulary_size, minibatch_size, num_hidden_output, input_tensors):
+		self.used_input_tensors = [tensor for tensor in input_tensors if not tensor.is_empty()]
+		self.input_feature_names = [tensor.name for tensor in self.used_input_tensors]
+		self.vocabularized_feature_names = [tensor.name for tensor in self.used_input_tensors if tensor.vocabularized]
+		self.num_hidden_output = num_hidden_output
+		self.y_vocabulary_size = y_vocabulary_size
+
+		n_attention = 0
+		for tensor in self.used_input_tensors:
+			n_attention += tensor.initialize_layers(rng, minibatch_size)
+
+		print("concatenated layers size: %i"%n_attention)
+
+		# output model
+		self.GRU = GRULayer(rng=rng, n_in=n_attention, n_out=num_hidden_output, minibatch_size=minibatch_size)  #DIKKAT
+		self.Wy = weights_const(num_hidden_output, self.y_vocabulary_size, 'Wy', 0)
+		self.by = weights_const(1, self.y_vocabulary_size, 'by', 0)
+
+		# attention model
+		self.Wa_h = weights_Glorot(num_hidden_output, n_attention, 'Wa_h', rng) # output model previous hidden state to attention model weights
+		self.Wa_c = weights_Glorot(n_attention, n_attention, 'Wa_c', rng) # contexts to attention model weights
+		self.ba = weights_const(1, n_attention, 'ba', 0)
+		self.Wa_y = weights_Glorot(n_attention, 1, 'Wa_y', rng) # gives weights to contexts
+
+		# Late fusion parameters
+		self.Wf_h = weights_const(num_hidden_output, num_hidden_output, 'Wf_h', 0)
+		self.Wf_c = weights_const(n_attention, num_hidden_output, 'Wf_c', 0)
+		self.Wf_f = weights_const(num_hidden_output, num_hidden_output, 'Wf_f', 0)
+		self.bf = weights_const(1, num_hidden_output, 'bf', 0)
+
+		self.params = [tensor.We for tensor in self.used_input_tensors if tensor.vocabularized]
+		self.params += [self.Wy, self.by,
+                        self.Wa_h, self.Wa_c, self.ba, self.Wa_y,
+                        self.Wf_h, self.Wf_c, self.Wf_f, self.bf]
+		for tensor in self.used_input_tensors:
+			if tensor.bidirectional:
+				self.params += tensor.GRUs[0].params
+				self.params += tensor.GRUs[1].params
+			else:
+				self.params += tensor.GRUs[0].params
+
+		# recurrence functions
+		def output_recurrence(x_t, h_tm1, Wa_h, Wa_y, Wf_h, Wf_c, Wf_f, bf, Wy, by, context, projected_context):
+			# Attention model
+			h_a = T.tanh(projected_context + T.dot(h_tm1, Wa_h))
+			alphas = T.exp(T.dot(h_a, Wa_y))
+			alphas = alphas.reshape((alphas.shape[0], alphas.shape[1])) # drop 2-axis (sized 1)
+			alphas = alphas / alphas.sum(axis=0, keepdims=True)
+			weighted_context = (context * alphas[:,:,None]).sum(axis=0)
+
+			h_t = self.GRU.step(x_t=x_t, h_tm1=h_tm1)
+
+			# Late fusion
+			lfc = T.dot(weighted_context, Wf_c) # late fused context
+			fw = T.nnet.sigmoid(T.dot(lfc, Wf_f) + T.dot(h_t, Wf_h) + bf) # fusion weights
+			hf_t = lfc * fw + h_t # weighted fused context + hidden state
+
+			z = T.dot(hf_t, Wy) + by
+			y_t = T.nnet.softmax(z)
+
+			return [h_t, hf_t, y_t, alphas]
+
+		def create_bidi(GRU_forward, GRU_backward):
+			def bidirectional_recurrence(x_f_t, x_b_t, h_f_tm1, h_b_tm1):
+				h_f_t = GRU_forward.step(x_t=x_f_t, h_tm1=h_f_tm1)
+				h_b_t = GRU_backward.step(x_t=x_b_t, h_tm1=h_b_tm1)
+				return [h_f_t, h_b_t]
+			return bidirectional_recurrence
+
+		def create_unidi(GRU_layer):
+			def unidirectional_recurrence(p_t, h_p_tm1):
+				h_p_t = GRU_layer.step(x_t=p_t, h_tm1=h_p_tm1)
+				return h_p_t	#dikkat i changed this
+			return unidirectional_recurrence
+
+		concatenated_input_tensors = []
+
+		for tensor in self.used_input_tensors:
+			if tensor.vocabularized:
+				x = tensor.We[tensor.tensor.flatten()].reshape((tensor.tensor.shape[0], minibatch_size, tensor.size_emb))  
+			else:
+				x = tensor.tensor.flatten().reshape((tensor.tensor.shape[0], minibatch_size, 1))
+
+			if tensor.bidirectional:
+				bidi_input_recurrence = create_bidi(tensor.GRUs[0], tensor.GRUs[1])
+
+				[h_f_t, h_b_t], _ = theano.scan(fn=bidi_input_recurrence,
+		            sequences=[x, x[::-1]], # forward and backward sequences
+		            outputs_info=[tensor.GRUs[0].h0, tensor.GRUs[1].h0])
+
+				concatenated_input_tensors += [h_f_t, h_b_t[::-1]]
+			else:
+				unidi_input_recurrence = create_unidi(tensor.GRUs[0])
+
+				h_p_t, _ = theano.scan(fn=unidi_input_recurrence,
+		        	sequences=[x],
+		        	outputs_info=[tensor.GRUs[0].h0])
+
+				concatenated_input_tensors += [h_p_t]
+
+		context = T.concatenate(concatenated_input_tensors, axis=2) 
+		projected_context = T.dot(context, self.Wa_c) + self.ba
+
+		[_, self.last_hidden_states, self.y, self.alphas], _ = theano.scan(fn=output_recurrence,
+		sequences=[context[1:]], # ignore the 1st word in context, because there's no punctuation before that
+		non_sequences=[self.Wa_h, self.Wa_y, self.Wf_h, self.Wf_c, self.Wf_f, self.bf, self.Wy, self.by, context, projected_context],
+		outputs_info=[self.GRU.h0, None, None, None])
+
+		print("Number of parameters is %d" % sum(np.prod(p.shape.eval()) for p in self.params))
+
+		self.L1 = sum(abs(p).sum() for p in self.params)
+		self.L2_sqr = sum((p**2).sum() for p in self.params)
+
+	def cost(self, y):
+		num_outputs = self.y.shape[0]*self.y.shape[1] # time steps * number of parallel sequences in batch
+		output = self.y.reshape((num_outputs, self.y.shape[2]))
+		return -T.sum(T.log(output[T.arange(num_outputs), y.flatten()]))
+
+	def save(self, file_path, gsums=None, learning_rate=None, validation_ppl_history=None, best_validation_ppl=None, epoch=None, random_state=None):
+		import _pickle
+		input_tensors_info = [tensor.as_dict() for tensor in self.used_input_tensors]
+		state = {
+		    "type":                     	self.__class__.__name__,
+		    "num_hidden_output":			self.num_hidden_output,
+		    "input_feature_names":			self.input_feature_names,
+		    "vocabularized_feature_names": 	self.vocabularized_feature_names,
+		    "input_tensors_info":			input_tensors_info,
+		    "y_vocabulary_size":        	self.y_vocabulary_size,
+		    "params":                   	[p.get_value(borrow=True) for p in self.params],
+		    "gsums":                    	[s.get_value(borrow=True) for s in gsums] if gsums else None,
+		    "learning_rate":            	learning_rate,
+		    "validation_ppl_history":   	validation_ppl_history,
+		    "epoch":                    	epoch,
+		    "random_state":             	random_state
+		}
+
+		with open(file_path, 'wb') as f:
+			_pickle.dump(state, f)
---
+++ thepunctuator.py
@@ -0,0 +1,159 @@
+# coding: utf-8
+from __future__ import division
+import sys
+import os
+import codecs
+from optparse import OptionParser
+from utilities import *
+import themodel as models
+
+import theano
+import theano.tensor as T
+import numpy as np
+
+def restore_unsequenced_test_data(test_data_path, vocabulary_dict, predict_function, input_feature_names, sequence_length, output_text=None):
+	proscript_data = read_proscript(test_data_path)
+
+	i = 0
+	with codecs.open(output_text, 'w', 'utf-8') as f_out:
+		while True:
+			subsequence_words = proscript_data['word'][i: i + sequence_length]
+			subsequences = {feature_name: proscript_data[feature_name][i: i + sequence_length] for feature_name in input_feature_names if not feature_name in vocabulary_dict.keys()}
+			for feature_name in vocabulary_dict.keys():
+				vocabulary = vocabulary_dict[feature_name]
+				subsequences[feature_name] = [vocabulary.get(w, vocabulary[UNK]) for w in proscript_data[feature_name][i: i + sequence_length]]
+
+			predict_from = [to_array(subsequences[feature_name]) for feature_name in input_feature_names]
+			y = predict_function(*predict_from)
+
+			predicted_punctuation_sequence = [0] + [np.argmax(y_t.flatten()) for y_t in y]
+			#print(predicted_punctuation_sequence)
+
+			f_out.write(subsequence_words[0])
+
+			last_eos_idx = 0
+			punctuations = []
+			for y_t in y:
+
+				p_i = np.argmax(y_t.flatten())
+				#punctuation = reverse_punctuation_vocabulary[p_i]
+				punctuation = p_i
+
+				punctuations.append(punctuation)
+
+				if punctuation in EOS_PUNCTUATION_CODES:
+					last_eos_idx = len(punctuations) # we intentionally want the index of next element
+
+			if subsequence_words[-1] == END:
+				step = len(subsequence_words) - 1
+			elif last_eos_idx != 0:
+				step = last_eos_idx
+			else:
+				step = len(subsequence_words) - 1
+
+			for j in range(step):
+				if options.readable_format:
+					if punctuations[j] == 0:
+						f_out.write(" ")
+					else:
+						f_out.write(" " + PUNCTUATION_VOCABULARY[punctuations[j]] + " ")
+				else:
+					f_out.write(" " + PUNCTUATION_VOCABULARY[punctuations[j]] + " ")
+				if j < step - 1:
+					try: 
+						f_out.write(subsequence_words[1+j])
+					except:
+						f_out.write("<unk>")
+						print(subsequence_words[1+j])
+
+			if subsequence_words[-1] == END:
+				break
+
+			i += step
+
+def main(options):
+	if checkArgument(options.model_file):
+		model_file = options.model_file
+		print("Model: %s"%model_file)
+	else:
+		sys.exit("Model file path argument missing")
+
+	if checkArgument(options.input_proscript, isFile=True):
+		TEST_FILE = options.input_proscript
+		print("Test file: %s"%TEST_FILE)
+		if checkArgument(options.output):
+			OUTPUT_FILE = options.output
+			OUTPUT_DIR = None
+			print("Output file: %s"%OUTPUT_FILE)
+		else:
+			sys.exit("Output file path argument missing")
+	elif checkArgument(options.input_directory, isDir=True):
+		TEST_DIR = options.input_directory
+		print("Test directory: %s"%TEST_DIR)
+		if checkArgument(options.output, isDir=True, createDir=True):
+			OUTPUT_DIR = options.output
+			print("Output directory: %s"%OUTPUT_DIR)
+		else:
+			sys.exit("Output directory path argument missing")
+	else:
+		sys.exit("File or directory to punctuate is missing!")
+
+	vocabulary_dict = {}
+	if checkArgument(options.vocabulary_file):
+		WORD_VOCAB_FILE = options.vocabulary_file
+		word_vocabulary = read_vocabulary(WORD_VOCAB_FILE)
+		vocabulary_dict['word'] = word_vocabulary
+	if checkArgument(options.pos_vocabulary_file):
+		POS_VOCAB_FILE = options.pos_vocabulary_file
+		pos_vocabulary = read_vocabulary(POS_VOCAB_FILE)
+		vocabulary_dict['pos'] = pos_vocabulary
+
+	print("Loading model parameters...")
+	net, inputs, input_feature_names, _ = models.load(model_file, 1)
+	print("Trained with:")
+	print(input_feature_names)
+
+	print("Building model...")
+	predict = theano.function(inputs=inputs, outputs=net.y)
+
+	print("Generating punctuation...")
+	if not OUTPUT_DIR == None:
+		#punctuate all proscripts in directory
+		sample_file_list = os.listdir(TEST_DIR)
+		sample_file_list.sort()
+		for sample_filename in sample_file_list:
+			if sample_filename.endswith(".csv"):
+				TEST_FILE = os.path.join(TEST_DIR, sample_filename)
+				TEST_FILE_BASENAME = os.path.splitext(os.path.basename(TEST_FILE))[0]
+				TEST_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "%s.txt"%TEST_FILE_BASENAME)
+				restore_unsequenced_test_data( TEST_FILE,
+										  	   vocabulary_dict=vocabulary_dict,
+										  	   predict_function=predict, 
+										  	   input_feature_names=input_feature_names, 
+										  	   sequence_length=options.sequence_length,
+										  	   output_text=TEST_OUTPUT_FILE)
+		print("Predictions written to %s"%(OUTPUT_DIR))
+	else:
+		restore_unsequenced_test_data( TEST_FILE,
+								  	   vocabulary_dict=vocabulary_dict,
+								  	   predict_function=predict, 
+								  	   input_feature_names=input_feature_names, 
+								  	   sequence_length=options.sequence_length,
+								  	   output_text=TEST_OUTPUT_FILE)
+		print("Predictions written to %s"%OUTPUT_FILE)
+
+if __name__ == "__main__":
+	usage = "usage: %prog [-s infile] [option]"
+	parser = OptionParser(usage=usage)
+	parser.add_option("-m", "--model_file", dest="model_file", default=None, help="model filename", type="string")
+	parser.add_option("-v", "--vocabulary_file", dest="vocabulary_file", default=None, help="vocabulary file", type="string")
+	parser.add_option("-x", "--pos_vocabulary_file", dest="pos_vocabulary_file", default=None, help="pos vocabulary file", type="string")
+	parser.add_option("-i", "--input_proscript", dest="input_proscript", default=None, help="input proscript file (csv)", type="string")
+	parser.add_option("-d", "--input_directory", dest="input_directory", default=None, help="directory with all proscript (csv) files to punctuate", type="string")
+	parser.add_option("-o", "--output", dest="output", default=None, help="output file/directory to write predictions", type="string")
+	parser.add_option("-r", "--readable_format", dest="readable_format", default=True, help="flag if output is desired in human readable format", action='store_true')
+	parser.add_option("-s", "--sequence_length", dest="sequence_length", default=50, help="sequence length for punctuating", type="int")
+
+	(options, args) = parser.parse_args()
+
+	main(options)
